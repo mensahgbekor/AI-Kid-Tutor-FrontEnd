b326377459a3ec34fd22c11345767da26edf3e43
Believe Kwamitse <believeserwaa80@gmail.com>
2025-05-30 10:50:39 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+function App() {
+  const router = createBrowserRouter ([
+
+  ]);
+  
+
+  return <RouterProvider router={router}/>;
+    
+  
+}
+
+export default App;
